b519d7d11e1f34ded65389941505c9b920e45852
repkin81 <59411495+repkin81@users.noreply.github.com>
2026-09-07 18:12:18 +0300

    feat: add English OpenCart module language

---
+++ opencart-extension/upload/admin/language/en-gb/extension/module/ultra_opencart.php
@@ -0,0 +1,13 @@
+<?php
+$_['heading_title'] = 'Ultra OpenCart PRO Connector';
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Module settings saved.';
+$_['text_edit'] = 'Connection settings';
+$_['entry_status'] = 'Status';
+$_['entry_api_key'] = 'API key';
+$_['entry_log_limit'] = 'Log limit';
+$_['help_api_key'] = 'The key is used by Ultra OpenCart PRO core to access the store API.';
+$_['button_save'] = 'Save';
+$_['button_cancel'] = 'Cancel';
+$_['error_permission'] = 'You do not have permission to modify this module.';
+$_['error_api_key'] = 'API key must contain at least 32 characters.';
