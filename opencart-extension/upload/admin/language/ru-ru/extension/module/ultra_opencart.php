b5d0ecd50bcbd1a7f3f4a1e32418283ce692aa9b
repkin81 <59411495+repkin81@users.noreply.github.com>
2026-09-07 18:12:14 +0300

    feat: add Russian OpenCart module language

---
+++ opencart-extension/upload/admin/language/ru-ru/extension/module/ultra_opencart.php
@@ -0,0 +1,13 @@
+<?php
+$_['heading_title'] = 'Ultra OpenCart PRO Connector';
+$_['text_extension'] = 'Расширения';
+$_['text_success'] = 'Настройки модуля сохранены.';
+$_['text_edit'] = 'Настройки подключения';
+$_['entry_status'] = 'Статус';
+$_['entry_api_key'] = 'API key';
+$_['entry_log_limit'] = 'Лимит журнала';
+$_['help_api_key'] = 'Ключ используется ядром Ultra OpenCart PRO для доступа к API магазина.';
+$_['button_save'] = 'Сохранить';
+$_['button_cancel'] = 'Отмена';
+$_['error_permission'] = 'У вас нет прав для изменения модуля.';
+$_['error_api_key'] = 'API key должен содержать минимум 32 символа.';
